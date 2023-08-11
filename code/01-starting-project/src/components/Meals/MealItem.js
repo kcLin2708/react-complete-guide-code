cd95b571120d3ec9adab6ae4ec333e226d42b8e9
ri27luq <kaichun.lin@campus.lmu.de>
2023-08-11 08:49:14 +0200

    Modal added

--- code/01-starting-project/src/components/Meals/MealItem.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const MealItem = () => {
-  return <div></div>;
-};
-
-export default MealItem;
--- code/01-starting-project/src/components/UI/Button.js
+++ code/01-starting-project/src/components/UI/Button.js
@@ -3,7 +3,7 @@ import React from "react";
 const Button = (props) => {
   return (
     <>
-      <button></button>
+      <button>{props.children}</button>
     </>
   );
 };
