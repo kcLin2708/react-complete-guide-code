9e24d12d57ab075a5c1df73d7b8e57c0c73021ff
ri27luq <kaichun.lin@campus.lmu.de>
2023-08-16 19:37:38 +0200

    React.memo() and useCallback applied

--- code/01-starting-setup/src/App.js
+++ code/01-starting-setup/src/App.js
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, { useCallback, useState } from "react";
 
-import './App.css';
+import DemoOutput from "./components/Demo/DemoOutput";
+import Button from "./components/UI/Button/Button";
+import "./App.css";
 
 function App() {
+  const [showParagraph, setShowParagraph] = useState(false);
+
+  console.log("APP RUNNUNG!");
+
+  const toggleParagraphHandler = useCallback(() => {
+    setShowParagraph((prevShowParagraph) => !prevShowParagraph);
+  }, []); //setShowParagraph has no dependency!!!
+
   return (
     <div className="app">
       <h1>Hi there!</h1>
+      <DemoOutput />
+      <Button onClick={toggleParagraphHandler}>Toggle Paragraph!</Button>
     </div>
   );
 }
