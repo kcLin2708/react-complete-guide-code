7b2047b2952fae6c26b6938fe6c71c9f5a800973
ri27luq <kaichun.lin@campus.lmu.de>
2023-08-23 19:10:30 +0200

    Code refactoring

--- code/01-starting-project/src/components/SimpleInput.js
+++ code/01-starting-project/src/components/SimpleInput.js
@@ -1,32 +1,18 @@
-import { useState, useRef, useEffect } from "react";
+import { useState } from "react";
 
 const SimpleInput = (props) => {
-  const inputNameRef = useRef();
   const [enteredName, setEnteredName] = useState("");
-  // const [inputIsValid, setInputIsValid] = useState(false);
   const [enteredNameTouched, setEnterNameTouched] = useState(false);
 
   const enteredNameIsValid = enteredName.trim() !== "";
   const nameInputIsInValid = !enteredNameIsValid && enteredNameTouched;
 
-  // useEffect(() => {
-  //   if (inputIsValid) {
-  //     console.log("Input is valid!");
-  //   }
-  // }, [inputIsValid]);
-
   const nameInputChangeHandler = (event) => {
     setEnteredName(event.target.value);
-    // if (event.target.value.trim() !== "") {
-    //   setInputIsValid(true);
-    // }
   };
 
   const nameInputBlurHandler = (event) => {
     setEnterNameTouched(true);
-    // if (enteredName.trim() === "") {
-    //   setInputIsValid(false);
-    // }
   };
 
   const formSubmissonHandler = (event) => {
@@ -37,11 +23,6 @@ const SimpleInput = (props) => {
     if (!enteredNameIsValid) {
       return;
     }
-    // setInputIsValid(true);
-    // console.log(enteredName);
-    // const enteredValue = inputNameRef.current.value;
-    // console.log(enteredValue);
-
     setEnteredName("");
     // inputNameRef.current.value = " " NOT IDEA, DON'T MANIPULATE THE DOM!!!
     setEnterNameTouched(false);
@@ -58,7 +39,6 @@ const SimpleInput = (props) => {
         <input
           type="text"
           id="name"
-          ref={inputNameRef}
           onChange={nameInputChangeHandler}
           onBlur={nameInputBlurHandler}
           value={enteredName}
