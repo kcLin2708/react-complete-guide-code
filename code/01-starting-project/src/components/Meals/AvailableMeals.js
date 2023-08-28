4d24cd48d5ae9aadf9f3162e189405ce9f37b20d
ri27luq <kaichun.lin@campus.lmu.de>
2023-08-28 20:42:58 +0200

    form added

--- code/01-starting-project/src/components/Meals/AvailableMeals.js
+++ code/01-starting-project/src/components/Meals/AvailableMeals.js
@@ -5,12 +5,19 @@ import classes from "./AvailableMeals.module.css";
 
 const AvailableMeals = () => {
   const [meals, setMeals] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [httpError, setHttpError] = useState();
 
-  const fetchMealsHandler = useCallback(async () => {
-    try {
+  useEffect(() => {
+    const fetchMealsHandler = async () => {
       const response = await fetch(
         "https://react-http-519b0-default-rtdb.europe-west1.firebasedatabase.app/meals.json"
       );
+
+      if (!response.ok) {
+        throw new Error("Something went wrong!");
+      }
+
       const data = await response.json();
 
       const loadedMeals = [];
@@ -25,14 +32,14 @@ const AvailableMeals = () => {
       }
 
       setMeals(loadedMeals);
-    } catch (error) {
-      console.log(error.message);
-    }
-  }, []);
+      setIsLoading(false);
+    };
 
-  useEffect(() => {
-    fetchMealsHandler();
-  }, [fetchMealsHandler]);
+    fetchMealsHandler().catch((error) => {
+      setIsLoading(false);
+      setHttpError(error.message);
+    });
+  }, []);
 
   const mealsList = meals.map((meal) => (
     <MealItem
@@ -43,6 +50,22 @@ const AvailableMeals = () => {
       price={meal.price}
     />
   ));
+
+  if (isLoading) {
+    return (
+      <section className={classes.MealIsLoading}>
+        <p>Loading...</p>
+      </section>
+    );
+  }
+
+  if (httpError) {
+    return (
+      <section className={classes.MealsError}>
+        <p>{httpError}</p>
+      </section>
+    );
+  }
 
   return (
     <section className={classes.meals}>
