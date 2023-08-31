330cffefccfd622a4fa80293c2bdd778ac8a61d6
ri27luq <kaichun.lin@campus.lmu.de>
2023-08-31 14:02:53 +0200

    module completed

--- code/01-starting-project/src/App.js
+++ code/01-starting-project/src/App.js
@@ -1,9 +1,22 @@
-import Counter from './components/Counter';
+import { useSelector } from "react-redux";
 
+import { Fragment } from "react";
+
+import Header from "./components/Header";
+import Auth from "./components/Auth";
+import UserProfile from "./components/UserProfile";
+import Counter from "./components/Counter";
 
 function App() {
+  const isAuth = useSelector((state) => state.auth.isAuthenticated);
+
   return (
-    <Counter />
+    <Fragment>
+      <Header />
+      {!isAuth && <Auth />}
+      {isAuth && <UserProfile />}
+      <Counter />
+    </Fragment>
   );
 }
 
--- code/01-starting-project/src/store/index.js
+++ code/01-starting-project/src/store/index.js
@@ -1,33 +1,12 @@
-import { Component } from "react";
+import { configureStore } from "@reduxjs/toolkit";
 
-import { createStore } from "redux";
+import counterReducer from "./couter-slice";
+import authReducer from "./auth-slice";
 
-const initialState = { counter: 0, showCounter: true };
-const counterReuducer = (state = initialState, action) => {
-  // NEVER MUTATING STATE!!! state.counter++
-  // ALWAYS WRITE A NEW STATE!!!
-  if (action.type === "increment") {
-    return { counter: state.counter + 1, showCounter: state.showCounter };
-  }
+const store = configureStore({
+  reducer: { counter: counterReducer, auth: authReducer },
+});
 
-  if (action.type === "increase") {
-    return {
-      counter: state.counter + action.amount,
-      showCounter: state.showCounter,
-    };
-  }
-
-  if (action.type === "decrement") {
-    return { counter: state.counter - 1, showCounter: state.showCounter };
-  }
-
-  if (action.type === "toggle") {
-    return { showCounter: !state.showCounter, counter: state.counter };
-  }
-
-  return state;
-};
-
-const store = createStore(counterReuducer);
+export const counterActions = counterReducer.actions;
 
 export default store;
