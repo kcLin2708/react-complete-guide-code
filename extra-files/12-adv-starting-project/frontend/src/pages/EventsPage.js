836a3ff74c42bc534190043ad0fd04d3f90ca664
ri27luq <kaichun.lin@campus.lmu.de>
2023-09-07 09:37:22 +0200

    basics completed

--- extra-files/12-adv-starting-project/frontend/src/pages/EventsPage.js
+++ extra-files/12-adv-starting-project/frontend/src/pages/EventsPage.js
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 
-const EVENTS = [
+const DUMMY_EVENTS = [
   { id: "e1", title: "Event 1" },
   { id: "e2", title: "Event 2" },
   { id: "e3", title: "Event 3" },
@@ -11,9 +11,9 @@ const EventsPage = () => {
     <>
       <h1>Events Page</h1>
       <ul>
-        {EVENTS.map((event) => (
+        {DUMMY_EVENTS.map((event) => (
           <li key={event.id}>
-            <Link to={`${event.id}`}>{event.title}</Link>
+            <Link to={event.id}>{event.title}</Link>
           </li>
         ))}
       </ul>
---
+++ extra-files/12-adv-starting-project/frontend/src/pages/EventsRoot.js
@@ -0,0 +1,14 @@
+import { Outlet } from "react-router-dom";
+
+import EventNavigation from "../components/EventsNavigation";
+
+const EventsRootLayout = () => {
+  return (
+    <>
+      <EventNavigation />
+      <Outlet />
+    </>
+  );
+};
+
+export default EventsRootLayout;
